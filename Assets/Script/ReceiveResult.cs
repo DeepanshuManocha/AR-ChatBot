a493a7df5977c01360867cf76c22cf3cdd4a8053
DeepanshuManocha <dmanocha1509@gmail.com>
2020-05-17 17:45:57 +0530

    speech & reply command working

---
+++ Assets/Script/ReceiveResult.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReceiveResult : MonoBehaviour {
+
+    public List<string> inputData = new List<string>();
+    public List<string> reply = new List<string>();
+    
+    public GameObject inputDataInputField, replyInputField;
+    int i;
+	// Use this for initialization
+	void Start () 
+    {
+        //GameObject.Find("Text").GetComponent<Text>().text = "You need to be connected to Internet";
+        //inputData.Add("hi");
+        //inputData.Add("how are you");
+        //inputData.Add(inputDataText);
+        //reply.Add("hi deepanshu");
+        //reply.Add("I am fine,thankyou!");
+        //reply.Add(replyText);
+	}
+
+    public void EnterInputData()
+    {
+        inputData.Add(inputDataInputField.GetComponent<InputField>().text);
+    }
+
+    public void EnterReply()
+    {
+        reply.Add(replyInputField.GetComponent<InputField>().text);
+    }
+	
+    public void onActivityResult(string recognizedText){
+        char[] delimiterChars = {'~'};
+        string[] result = recognizedText.Split(delimiterChars);
+
+        //You can get the number of results with result.Length
+        //And access a particular result with result[i] where i is an int
+        //I have just assigned the best result to UI text
+        GameObject.Find("STT").GetComponent<Text>().text = result[0];
+
+        for (i = 0; i < reply.Count; i++) 
+        {
+            if(result[0]==inputData[i])
+            {
+                GameObject.Find("TTS").GetComponent<TextToSpeech>().inputText=reply[i];
+            }
+            
+        
+        }
+        //i=0;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/TextToSpeech.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextToSpeech : MonoBehaviour
+{
+    
+    public AudioSource _audio;
+    public string inputText;
+    public string input;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audio=gameObject.GetComponent<AudioSource>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        for(int i=0; i<(GameObject.Find("Camera").GetComponent<ReceiveResult>().reply.Count); i++)
+        {
+            if(inputText==(GameObject.Find("Camera").GetComponent<ReceiveResult>().reply[i]))
+            {
+                StartCoroutine (DownloadTheAudio());
+                inputText=null;
+            } 
+        }
+    }
+
+    IEnumerator DownloadTheAudio()
+    {
+        string url="https://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q="+inputText+"&tl=En-gb";
+        WWW www= new WWW (url);
+        yield return www;
+
+        _audio.clip = www.GetAudioClip (false, true, AudioType.MPEG);
+        _audio.Play ();
+    }
+
+    public void ButtonClick()
+    {
+            
+    }
+}
